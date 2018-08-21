37edd6f8e6f9cb3fa0cdf3dada37412c8d5a408a
ratnania <ratnaniahmed@gmail.com>
2018-08-21 08:29:48 +0200

    2d est test

--- symgp/tests/test_est_2d.py
+++ symgp/tests/test_est_2d.py
@@ -4,6 +4,7 @@ from sympy import Tuple
 from sympy import lambdify
 
 from symfe import dx, dy, Unknown, Constant
+from symfe.printing import latex
 
 from symgp.kernel import NLML
 from symgp.nelder_mead import nelder_mead
@@ -118,7 +119,7 @@ def test_est_2d_2():
     eps = 1.e-5
 #    eps = 1.e-4
 
-    niter = 5
+    niter = 10
 #    niter = 1
     phis = []
     scores = []
@@ -158,7 +159,124 @@ def test_est_2d_2():
     print(scores)
     print(phis)
 
+def test_est_2d_3():
+    u = Unknown('u', ldim=2)
+    phi = Constant('phi')
+
+    # ... define a partial differential operator as a lambda function
+    L = lambda u: phi*u + dx(u) + dy(dy(u))
+    L_expected = lambda u: 2.*u + dx(u) + dy(dy(u))
+    # ...
+
+    # ... symbolic functions for unknown and rhs
+    from sympy.abc import x, y
+    from sympy import sin, cos
+
+    u_sym = x**2 + y
+    f_sym = L_expected(u_sym)
+    # ...
+
+    # ... lambdification + evaluation
+    from numpy import linspace, meshgrid, zeros
+    from numpy.random import rand
+
+    u_num = lambdify((x,y), u_sym, "numpy")
+    f_num = lambdify((x,y), f_sym, "numpy")
+
+    x_u = rand(50,2)
+
+    x_f = x_u
+
+    us = u_num(x_u[:,0], x_u[:,1])
+    fs = f_num(x_f[:,0], x_f[:,1])
+    # ...
+
+    # compute the likelihood
+#    nlml = NLML(L(u), u, 'SE')
+    nlml = NLML(L(u), u, 'RBF')
+
+    # set values
+    nlml.set_u(x_u, us)
+    nlml.set_f(x_f, fs)
+
+    from numpy.random import rand, normal
+    from numpy import exp, ones, log
+
+
+    # ... using pure python implementation
+    from symgp.nelder_mead import nelder_mead
+
+    phi_expected = 2.
+    phi_var = 0.5
+
+    n_samples = 1000
+    x_starts = rand(len(nlml.args), n_samples)
+    i_phi = list(nlml.args).index('phi')
+    x_starts[i_phi,:] = log(normal(phi_expected, phi_var, n_samples))
+
+    phis = []
+    scores = []
+    phiso = []
+    scoreso = []
+    for i in range(0, n_samples):
+        print('> sample ', i)
+
+        x_start = x_starts[:,i]
+
+        # ...
+        def f(params):
+            params[i_phi] = x_starts[i_phi,i]
+            return nlml(params)
+
+        m = nelder_mead(f, x_start,
+                        step=0.1, no_improve_thr=10e-4,
+                        no_improv_break=4,
+                        max_iter=0, alpha=.5, gamma=1.5, rho=-0.5, sigma=0.5,
+                        verbose=False)
+
+        args = exp(m[0])
+        score = m[1]
+
+        phis.append(nlml.map_args(args)['phi'])
+        scores.append(score)
+        # ...
+
+        # ...
+        m = nelder_mead(nlml, x_start,
+                        step=0.1, no_improve_thr=10e-4,
+                        no_improv_break=4,
+                        max_iter=0, alpha=.5, gamma=1.5, rho=-0.5, sigma=0.5,
+                        verbose=False)
+
+        args = exp(m[0])
+        score = m[1]
+
+        phiso.append(nlml.map_args(args)['phi'])
+        scoreso.append(score)
+        # ...
+
+    from numpy import savetxt
+    savetxt('est_2d/phis.txt', asarray(phis))
+    savetxt('est_2d/scores.txt', asarray(scores))
+    savetxt('est_2d/phiso.txt', asarray(phiso))
+    savetxt('est_2d/scoreso.txt', asarray(scoreso))
+
+    plt.plot(phis, scores, '.b', label=r'fixed $\phi$', alpha=0.4)
+    plt.plot(phiso, scoreso, '.r', label=r'free $\phi$', alpha=0.4)
+    plt.axvline(x=phi_expected, color='green', alpha=0.5)
+
+    plt.xlabel(r'$\phi$')
+    plt.ylabel(r'Likelihood $\mathcal{L}$')
+    title = '$L u := {}$'.format(latex(L(u)))
+    title += '\n'
+    title += r'$\phi_{exact}'+' := {}$'.format(phi_expected)
+    plt.title(title)
+    plt.legend()
+    plt.show()
+    # ...
+
 ######################################
 if __name__ == '__main__':
 #    test_est_2d_1()
-    test_est_2d_2()
+#    test_est_2d_2()
+    test_est_2d_3()
